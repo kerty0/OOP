ed675f58999f0f611cd43034531f72f111c97fa2
kerty <g.kabakov@inbox.ru>
2025-04-09 23:50:52 +0300

    l2 solution

--- l2.cpp
+++ l2.cpp
@@ -50,3 +50,38 @@ class Monster {
                   << ", Attack: " << attack << ", Defense: " << defense << std::endl;
     }
 };
+
+class Weapon {
+   private:
+    std::string name;
+    int attack;
+    int weight;
+
+   public:
+    // Конструктор
+    Weapon(const std::string& n, int a, int w)
+        : name(n), attack(a), weight(w) {
+        std::cout << "Weapon " << name << " created!\n";
+    }
+
+    // Деструктор
+    ~Weapon() {
+        std::cout << "Weapon " << name << " destroyed!\n";
+    }
+
+    void displayInfo() const {
+        std::cout << "Weapon name: " << name << ", Attack: " << attack << ", Weight: " << weight << std::endl;
+    }
+};
+
+int main() {
+    Character hero("Hero", 100, 20, 10);
+    Weapon sword("Great sword", 100, 50);
+    Monster goblin("Goblin", 50, 15, 5);
+    Weapon dagger("Small dagger", 1, 1);
+    hero.displayInfo();
+    sword.displayInfo();
+    goblin.displayInfo();
+    dagger.displayInfo();
+    return 0;
+}

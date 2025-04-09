5c16fd967e1259d4f41c3e09f136db6b3ede5d1b
kerty <g.kabakov@inbox.ru>
2025-04-09 23:41:31 +0300

    l2

---
+++ l2.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <string>
+
+class Character {
+   private:
+    std::string name;
+    int health;
+    int attack;
+    int defense;
+
+   public:
+    // Конструктор
+    Character(const std::string& n, int h, int a, int d)
+        : name(n), health(h), attack(a), defense(d) {
+        std::cout << "Character " << name << " created!\n";
+    }
+
+    // Деструктор
+    ~Character() {
+        std::cout << "Character " << name << " destroyed!\n";
+    }
+
+    void displayInfo() const {
+        std::cout << "Name: " << name << ", HP: " << health
+                  << ", Attack: " << attack << ", Defense: " << defense << std::endl;
+    }
+};
+
+class Monster {
+   private:
+    std::string name;
+    int health;
+    int attack;
+    int defense;
+
+   public:
+    // Конструктор
+    Monster(const std::string& n, int h, int a, int d)
+        : name(n), health(h), attack(a), defense(d) {
+        std::cout << "Monster " << name << " created!\n";
+    }
+
+    // Деструктор
+    ~Monster() {
+        std::cout << "Monster " << name << " destroyed!\n";
+    }
+
+    void displayInfo() const {
+        std::cout << "Name: " << name << ", HP: " << health
+                  << ", Attack: " << attack << ", Defense: " << defense << std::endl;
+    }
+};

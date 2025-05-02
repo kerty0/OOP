950c64b672be056385b34cfe05dee8bd7df0ca58
kerty <g.kabakov@inbox.ru>
2025-05-03 00:09:45 +0300

    l6 solution

--- l6.cpp
+++ l6.cpp
@@ -17,11 +17,39 @@ class GameManager {
     }
 };
 
+template <typename T>
+class Queue {
+   private:
+    std::vector<T> items;
+
+   public:
+    void push(const T& item) {
+        items.push_back(item);
+    }
+
+    T pop() {
+        if (items.empty()) {
+            throw std::out_of_range("pop() called on empty queue");
+        }
+
+        T pop = items[0];
+        items.erase(items.begin());
+        return pop;
+    }
+};
+
 int main() {
     try {
         GameManager<Entity*> manager;
         manager.addEntity(new Player("Hero", -100, 0));  // Вызовет исключение
     } catch (const std::invalid_argument& e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+    }
+
+    try {
+        Queue<int> queue;
+        queue.pop();  // Вызовет исключение
+    } catch (const std::out_of_range& e) {
         std::cerr << "Error: " << e.what() << std::endl;
     }
 
